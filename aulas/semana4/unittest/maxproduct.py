a25cdc5487afe0e28981a9b65928316d0d82889b
pedrohsbarbosa99 <pedrohsbarbosa99@gmail.com>
2021-02-27 15:27:29 -0300

    adicionando aulas semana4

--- aulas/semana4/unittest/maxproduct.py
+++ aulas/semana4/unittest/maxproduct.py
@@ -1,7 +1,3 @@
-"""
-* S4V2 - Testes
-
-"""
 def max_product(*args):
     lista_numeros = list(args)
     lista_numeros = [float(numero) for numero in lista_numeros]
@@ -9,9 +5,3 @@ def max_product(*args):
     maior_valor = lista_numeros[-1]
     segundo_maior_valor = lista_numeros[-2]
     return maior_valor * segundo_maior_valor
-
-
-assert max_product(1,2,3,4,5) == 20
-assert max_product(1,5,0,2) == 10
-assert max_product(1,5,0,2,20,10) == 200
-assert max_product(1,10,0,2,20,3) == 200

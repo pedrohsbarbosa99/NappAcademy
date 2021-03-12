b5c28f557bda2e3a2f7eaf2ba79937f90513b7e1
pedrohsbarbosa99 <pedrohsbarbosa99@gmail.com>
2021-03-11 22:31:33 -0300

    Adicionando exercicio semana6

--- exercicios/semana6/BancoNapp/contas/ContaPoupanca.py
+++ exercicios/semana6/BancoNapp/contas/ContaPoupanca.py
@@ -6,32 +6,13 @@ class ContaPoupanca(Conta):
       self.limite = kwargs.setdefault('limite', 0)
       super(ContaPoupanca, self).__init__(**kwargs)
 
+
    def rendimento_aniversario(self, juros):
       if juros < 0 or juros > 1:
-          raise ValueError('Os juros precisam ser entre 0 (0%) e 1 (100%).')
+         raise ValueError('Os juros precisam ser entre 0 (0%) e 1 (100%).')
       juros = self.saldo * juros
       self.saldo = self.saldo + juros
       return self.saldo
-
-   def deposito(self, valor):
-        """
-        Método para realizar depósito.
-        Este método suporta somente números maiores que zero.
-
-        Args:
-            valor (float ou int): Valor positivo do depósito
-
-        Raises:
-            ValueError: Erro ocorre quando é informado valor negativo.
-            TypeError: Quando o tipo passado não for inteiro ou float.
-        """
-        if isinstance(valor, (float, int)):
-            if valor <= 0:
-                raise ValueError('Valor do depósito precisa ser maior que zero')
-            self.saldo = self.saldo + valor
-            self.extrato.append(('D', valor))
-            return
-        raise TypeError('O depósito precisa ser numérico')
 
    def saque(self, valor):
       """
@@ -42,7 +23,7 @@ class ContaPoupanca(Conta):
          valor (float ou int): Valor positivo do saque
 
       Raises:
-         ValueError: Erro ocorre quando é informado valor negativo.
+         ValueError: Erro ocorre quando é informado valor negativo ou maior que 1.
          TypeError: Quando o tipo passado não for inteiro ou float.
 
       Returns:
@@ -56,12 +37,3 @@ class ContaPoupanca(Conta):
          self.extrato.append(('S', valor))
          return valor
       raise TypeError('O valor do saque precisa ser numérico')
-
-   def get_extrato(self):
-      """
-      Retorna a lista dos saques e depósitos feitos na conta.
-
-      Returns:
-         List: Lista de operações
-      """
-      return self.extrato

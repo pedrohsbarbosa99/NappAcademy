c6695c524dae8c5a4aab34e2d8187ee922fa9b48
pedrohsbarbosa99 <pedrohsbarbosa99@gmail.com>
2021-03-18 18:52:56 -0300

    Implementando a classe CLiente e seu teste

---
+++ semana7/ecommerce/testes/test_Cliente.py
@@ -0,0 +1,22 @@
+from ecommerce.classes.Cliente import Cliente
+import pytest
+
+class TestClient:
+    def test_class_declared(self):
+        objeto = Cliente('John Doe')
+        assert isinstance(objeto, Cliente)
+
+    def test_instanciar_objeto(self):
+        objeto = Cliente('Jose da Silva')
+        assert objeto.nome, 'Jose da Silva'
+    
+    def test_setter(self):
+        objeto = Cliente('Jose da Silva')
+        assert objeto.nome == 'Jose da Silva'
+        objeto.nome = 'Jose da Silva Junior'
+        assert objeto.nome == 'Jose da Silva Junior'
+    
+    def test_str_repr(self):
+        cliente = Cliente('Jose da Silva')
+        assert str(cliente) == 'Jose da Silva'
+        assert repr(cliente) == 'Cliente => Jose da Silva'

87aff9ccf27743d766c1a6e2a2f234fd282f944b
pedrohsbarbosa99 <pedrohsbarbosa99@gmail.com>
2021-05-12 20:10:08 -0300

    deletando pasta nao usada

--- aulas/semana4/unittest/maxproduct.py
+++
@@ -1,7 +0,0 @@
-def max_product(*args):
-    lista_numeros = list(args)
-    lista_numeros = [float(numero) for numero in lista_numeros]
-    lista_numeros.sort()
-    maior_valor = lista_numeros[-1]
-    segundo_maior_valor = lista_numeros[-2]
-    return maior_valor * segundo_maior_valor

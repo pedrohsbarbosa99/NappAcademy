ec857a189b6143c35bcc5db2e0c754be58f19abf
pedrohsbarbosa99 <pedrohsbarbosa99@gmail.com>
2021-06-11 19:20:47 -0300

    realizando ajustes semana04

--- semana04/exercicio08/test_exercicio08.py
+++ semana04/exercicio08/test_exercicio08.py
@@ -1,16 +1,16 @@
 import unittest
 from exercicio08 import juros_compostos
 
-class CalculoJurosCompostosTestCas(unittest.TestCase):
+
+class CalculoJurosCompostosTestCase(unittest.TestCase):
     def test_calculo_juros_cenario_1(self):
-        self.assertEqual(259.37, juros_compostos(100, 10, 10))
+        self.assertEqual(259.38, juros_compostos(100, 10, 10))
         self.assertNotEqual(259.38, juros_compostos(100, 10, 10))
 
     def test_calculo_juros_cenario_2(self):
         self.assertEqual(259.37, juros_compostos(100, 10, '10'))
         self.assertEqual(259.37, juros_compostos('100', '10', '10'))
-    
+
     def test_wrong_cenario_1(self):
         with self.assertRaises(ValueError):
             juros_compostos(1, 'a', 10)
-            
